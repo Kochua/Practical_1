6af6b01ef32ac529ebbf86ce63af40cb0b291a29
takometonidze01 <takometonidze2012@gmail.com>
2024-06-12 14:25:16 +0400

    Initial Commit

---
+++ Practical 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical 1
+//
+//  Created by Tako Metonidze on 12.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
